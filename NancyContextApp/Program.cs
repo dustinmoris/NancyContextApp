725a58f0ad43da3ee3695616a9a217514f7a6a4b
Dustin Gorski <dustin.gorski@gamesys.co.uk>
2016-06-29 16:54:23 +0100

    Upload solution

---
+++ NancyContextApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+using Nancy.TinyIoc;
+
+namespace NancyContextApp
+{
+    public interface IDependency
+    {
+        NancyContextWrapper ContextWrapper { get; }
+    }
+
+    public class Dependency : IDependency
+    {
+        public Dependency(NancyContextWrapper contextWrapper)
+        {
+            ContextWrapper = contextWrapper;
+        }
+
+        public NancyContextWrapper ContextWrapper { get; }
+    }
+
+    public class NancyContextWrapper
+    {
+        public NancyContextWrapper(NancyContext context)
+        {
+            Context = context;
+        }
+
+        public readonly NancyContext Context;
+    }
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule(IDependency dependency)
+        {
+            Get["/"] = _ => "Hello World" + dependency.ContextWrapper.Context.Request.Query["test"];
+        }
+    }
+
+    public class DefaultBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
+
+            container.Register((c, p) => new NancyContextWrapper(context));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:8888/")))
+            {
+                host.Start();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
